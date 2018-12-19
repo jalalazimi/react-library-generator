a9359c0486e9c851cea3389248464c7c0981074d
Jalal azimi <jalalazimi@gmail.com>
2018-12-19 20:48:48 +0330

    update npm name checker title

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const process = require('process')
 
 //  Check Node version
 if (semver.satisfies(process.versions.node, '>=8')) {
-  require('./bin')
+  require('./lib')
 } else {
   console.log('Incorrect Node version')
   process.exit()
