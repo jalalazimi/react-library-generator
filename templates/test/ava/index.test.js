74d0a3afb039039cc4f68000a47b1d130aac593a
Jalal azimi <jalalazimi@gmail.com>
2018-12-25 15:53:10 +0330

    create ava test file

---
+++ templates/test/ava/index.test.js
@@ -0,0 +1,17 @@
+// @flow
+import test from 'ava'
+import React from 'react'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Component from '../src'
+
+configure({ adapter: new Adapter() })
+
+test('can use ava', (t) => {
+	t.pass()
+})
+
+test('rendered div element', (t) => {
+	const wrapper = shallow(<Component/>)
+	t.is(wrapper.type(), 'div')
+})
